17d46d93f48dbb0852def30f42579bdec9154892
Sarah Dunford <sarahbd97@gmail.com>
2020-08-24 19:42:06 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,8 @@
 def square_array(array)
   # your code here
   array.each do |index|
-    array[index] = array[index]**2
+    value = array[index]
+    final_value = value**2
+    final value
   end
 end
