58c1a8973ae077153cb3599482cbdc4eef59dfdb
Sarah Dunford <sarahbd97@gmail.com>
2020-08-24 19:47:58 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   # your code here
-  array.each do |index|
-    value = array[index]
-    final_value = value ** 2
-    final_value
+  new_array = []
+  array.each do |value|
+    new_array.push(value ** 2)
   end
+  return new_array
 end
