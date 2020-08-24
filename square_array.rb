9b59c2675877630d2c03b304d2ceb3cffc8ebc19
Sarah Dunford <sarahbd97@gmail.com>
2020-08-24 19:58:37 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,16 @@
-def square_array(array)
+def quare_array(array)
   # your code here
   new_array = []
   array.each do |value|
     new_array << value ** 2
   end
   return new_array
+end
+
+def square_array(array)
+  # your code here
+  array.collect do |value|
+    array == value ** 2
+  end
+  return array
 end
